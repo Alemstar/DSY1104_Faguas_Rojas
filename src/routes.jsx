f4bf0d489c398fad93e1226c2e80c7c524128703
PV-Alumno <PV-Alumno@duoc.acad>
2025-10-13 20:05:44 -0300

    limpieza de archivo routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,6 @@
 import { createBrowserRouter } from 'react-router'
 import Root from './pages/root'
 import Home from './pages/Home'
-    //import Home from './pages/home/index'
-    //import Products from './pages/products/index'
-    //import { productsLoader } from './loaders/products'
-    //import { homeLoader } from './loaders/home'
 
 export const router = createBrowserRouter([
   {
